0bb40eae01b1515ee49f40b6343ab368960a732a
Domenica Vizcarra <domeviz3410@gmail.com>
2023-08-09 19:47:05 -0500

    Autorizacion

---
+++ src/main/java/com/uce/edu/demo/PwAutorizacionU4P4DvApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAutorizacionU4P4DvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAutorizacionU4P4DvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uce/edu/demo/PwAutorizacionU4P4DvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwAutorizacionU4P4DvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
